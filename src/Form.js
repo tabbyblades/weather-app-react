282598354dc8a70505963cf319f37159b14fd7f6
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-09-25 15:26:08 +0900

    Updated the search form

--- src/Form.js
+++ src/Form.js
@@ -3,23 +3,10 @@ import "./Form.css";
 
 export default function Form() {
   return (
-    <div className="Form">
-      <nav className="navbar navbar-light">
-        <form className="form-inline">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Location"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
+        <form className="form">
+          <input type= "search" className="searchForm"/>
+           <input type ="submit" value="search" className="submitButton"/>
         </form>
-      </nav>
-    </div>
+
   );
 }
