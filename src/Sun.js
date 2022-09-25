b374e8f63b8ddf4253c397e5bcc11c6b8fd17909
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-09-25 09:13:18 +0900

    Created Sun.js

---
+++ src/Sun.js
@@ -0,0 +1,10 @@
+import React from "react";
+import sun from "../images/01d.svg";
+
+export default function Sun() {
+  return (
+    <div className="Sun">
+      <img src={sun} alt="Sun" height={250} />
+    </div>
+  );
+}
