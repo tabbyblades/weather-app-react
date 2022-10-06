aaad05f42050b7b1e86aa35d98f8ac0cbe703d21
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-10-07 07:44:51 +0900

    Updated date in WeatherForecastDay

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,11 +2,18 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay (props){
+
+    function day (){
+        let date = new Date (props.data.dt * 1000);
+        let day = date.getDay();
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        return days[day];
+    }
     return(
         <div className="WeatherForecast">
             <div className="row">
                 <div className="col-sm-2">
-                    <div className="WeatherForecast-day">{props.data.dt}</div>
+                    <div className="WeatherForecast-day">{day()}</div>
                     <WeatherIcon code={props.data.weather[0].icon} height={100}/>
                     <div className="WeatherForecast-temps">
                     <span className="WeatherForecast-temp-min">{Math.round(props.data.temp.min)}°C</span>
